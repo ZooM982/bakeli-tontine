d1ea00af3ae2fe084e3221112b8f5ecc4cec7974
ZooM98 <haurlyroll@gmail.com>
2024-05-20 19:25:08 +0000

    design for forgot password files

--- src/components/Authentification/ForgotPassword.js
+++ src/components/Authentification/ForgotPassword.js
@@ -3,27 +3,36 @@ import React from "react";
 import { auth } from "../FirebaseConfig";
 import { useNavigate } from "react-router-dom";
 import "../Authentification/ForgotPassword.css"
+import ForgotImg from "../../images/forgotpwrd.png"
 
-function ForgotPassword(){
+function ForgotPassword() {
     const history = useNavigate();
 
-    const handleSubmit = async(e)=>{
+    const handleSubmit = async (e) => {
         e.preventDefault()
         const emalVal = e.target.email.value;
-        sendPasswordResetEmail(auth,emalVal).then(data=>{
+        sendPasswordResetEmail(auth, emalVal).then(data => {
             alert("Check your gmail")
             history("/")
-        }).catch(err=>{
+        }).catch(err => {
             alert(err.code)
         })
     }
-    return(
+    return (
         <div className="App">
-            <h1>Forgot Password</h1>
-            <form onSubmit={(e)=>handleSubmit(e)}>
-                <input name="email" placeholder="Entrer votre adresse mail" /><br/><br/>
-                <button type="submit" className="btn">Reset</button>
-            </form>
+            <div className="left-side">
+                <img src={ForgotImg} alt="..." />
+            </div>
+            <div className="right-side">
+                <div className="cadre">
+                    <h1>Mot de passse oublié</h1>
+                    <p>Pour réinitialiser votre mot de passe entrez votre e-mail ou votre numéro de téléphone</p>
+                    <form onSubmit={(e) => handleSubmit(e)}>
+                        <input name="email" placeholder="N° de téléphone ou e-mail" /><br /><br />
+                        <button type="submit" className="btn-forget">Envoyer</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
